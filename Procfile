51117ce47febfc0a7763511aee0e1f9ccaef5a1f
Mark Obukhov <obukhov.mark.illich@gmail.com>
2026-05-19 13:38:35 +0300

    add Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: uvicorn app:create_app --host 0.0.0.0 --port $PORT --factory
--- run.py
+++ run.py
@@ -1,7 +1,9 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
